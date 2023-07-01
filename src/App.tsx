214f98d93879a5fb6386ebbe49e9da7fe501cb16
makaranddhamale <makarand.dhamale09@gmail.com>
2023-07-01 15:57:25 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import { Todo } from "./components/model";
+import TodoList from "./components/TodoList";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  const onDragEnd = (result: DropResult) => {
+    console.log(result);
+    const { source, destination } = result;
+    if (!destination) return;
+
+    console.log("1");
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+
+    console.log("2");
+
+    let add;
+    let active = todos;
+    let complete = completedTodos;
+
+    console.log("active ", active);
+    console.log("completed", complete);
+
+    if (source.droppableId === "TodosList") {
+      add = active[source.index];
+      active.splice(source.index, 1);
+    } else {
+      add = complete[source.index];
+      complete.splice(source.index, 1);
+    }
+
+    console.log("active ", active);
+    console.log("completed", complete);
+
+    if (destination.droppableId === "TodosList") {
+      active.splice(destination.index, 0, add);
+    } else {
+      complete.splice(destination.index, 0, add);
+    }
+
+    console.log("active ", active);
+    console.log("completed", complete);
+    console.log("add", add);
+
+    setCompletedTodos(complete);
+    setTodos(active);
+  };
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="App">
+        <span className="heading">Taskify</span>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+        <TodoList
+          todos={todos}
+          setTodos={setTodos}
+          completedTodos={completedTodos}
+          setCompletedTodos={setCompletedTodos}
+        />
+      </div>
+    </DragDropContext>
+  );
+};
+
+export default App;
